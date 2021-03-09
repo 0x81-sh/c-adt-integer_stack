6b47e5924c8c968dc91d88aca4bdb46fd01f06a8
Ulrich Barnstedt <ulrich.barnstedt+git@gmail.com>
2021-03-09 10:50:27 +0100

    Fix commit

--- whc/limits.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "fraction.h"
-
-typedef struct Limits *PLimits;
-
-PLimits Limits_create();
-void Limits_check(PLimits _this, PFraction newV);
-void Limits_print(PLimits _this);
